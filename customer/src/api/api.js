a7bbf491cd22bf39777ee829f2a5ca770aa4dc0f
shittake <shittake@shittake.shittake>
2025-09-16 09:00:23 +0530

    customerla url uploaded to fabfab.onrender.com

--- customer/src/api/api.js
+++ customer/src/api/api.js
@@ -1,5 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";  
-
+const BASE_URL = "https://fabfab.onrender.com"
 // ---------------- AUTH ----------------
 export const signup = async (data) => {
   const res = await fetch(`${BASE_URL}/auth/signup`, {
